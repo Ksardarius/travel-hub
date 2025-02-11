986afeffecafb5b5772467ee84ca2e5f392fe7da
Mihails Orlovs <morlovs@eisgroup.com>
2025-02-11 02:58:24 +0000

    added declaration maps

--- src/CookingApp/packages/gateway-api/tsup.config.ts
+++ src/CookingApp/packages/gateway-api/tsup.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
     entry: ['src/index.ts'],
     // Enable TypeScript type definitions to be generated in the output.
     // This provides type-definitions to consumers.
-    dts: true,
+    dts: false,
     // Clean the `dist` directory before building.
     // This is useful to ensure the output is only the latest.
     clean: true,
